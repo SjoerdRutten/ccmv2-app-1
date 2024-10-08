2242629359fd9017401b33c2ea9cf2cc68e1c5d9
Ray.nl B.V <sjoerd@ray.nl>
2024-10-08 16:50:43 +0200

    UI

--- src/Ccm/views/components/tables/delete-link.blade.php
+++ src/Ccm/views/components/tables/delete-link.blade.php
@@ -1,4 +1,4 @@
 <x-ccm::buttons._icon_button icon="heroicon-s-trash"
-                             class="text-red-400"
+                             class="text-red-500"
         {{ $attributes }}
 ></x-ccm::buttons._icon_button>
