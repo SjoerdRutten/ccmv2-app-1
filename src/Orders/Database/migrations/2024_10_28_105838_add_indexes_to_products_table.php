e2081927e48e99451959998a9311a787b45b3df0
Ray.nl B.V <sjoerd@ray.nl>
2024-10-28 12:01:33 +0100

    Add indexes

--- src/Orders/Database/migrations/2024_10_28_105838_add_indexes_to_products_table.php
+++ src/Orders/Database/migrations/2024_10_28_105838_add_indexes_to_products_table.php
@@ -10,7 +10,6 @@ return new class extends Migration
     {
         Schema::table('products', function (Blueprint $table) {
             $table->index(['sku']);
-            $table->index(['ean']);
         });
 
         Schema::table('order_rows', function (Blueprint $table) {
