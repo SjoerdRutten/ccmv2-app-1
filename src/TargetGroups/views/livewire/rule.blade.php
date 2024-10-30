7b7184507d755c1557c708ccb1ced9fd7e75e1d3
Ray.nl B.V <sjoerd@ray.nl>
2024-10-30 10:29:52 +0100

    Equals many for integer

--- src/TargetGroups/views/livewire/rule.blade.php
+++ src/TargetGroups/views/livewire/rule.blade.php
@@ -91,17 +91,19 @@
                 <option value="gte">Groter of gelijk</option>
                 <option value="lt">Kleiner dan</option>
                 <option value="lte">Kleiner of gelijk</option>
+                <option value="between">Tussen</option>
                 <option value="eq">Gelijk aan</option>
                 <option value="ne">Niet gelijk aan</option>
-                <option value="between">Tussen</option>
+                <option value="eqm">Gelijk aan 1 van</option>
+                <option value="neqm">Niet gelijk aan 1 van</option>
             </x-ccm::forms.select>
 
             @if (Arr::get($filter, 'operator'))
                 @if (Arr::get($filter, 'operator') === 'between')
-                    <x-ccm::forms.input type="number" step="1" name="filter.from" wire:model.blur="filter.from"/>
-                    <x-ccm::forms.input type="number" step="1" name="filter.to" wire:model.blur="filter.to"/>
+                    <x-ccm::forms.input type="text" step="1" name="filter.from" wire:model.blur="filter.from"/>
+                    <x-ccm::forms.input type="text" step="1" name="filter.to" wire:model.blur="filter.to"/>
                 @else
-                    <x-ccm::forms.input type="number" step="1" name="filter.value" wire:model.blur="filter.value"/>
+                    <x-ccm::forms.input type="text" step="1" name="filter.value" wire:model.blur="filter.value"/>
                 @endif
             @endif
         @elseif (Arr::get($filter, 'columnType') === 'date')
