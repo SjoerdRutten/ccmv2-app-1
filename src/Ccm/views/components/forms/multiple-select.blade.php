2fb7a578153db0a35b463950d5ce8af9a5f95d2f
Ray.nl B.V <sjoerd@ray.nl>
2024-11-04 16:33:15 +0100

    Design multi select

--- src/Ccm/views/components/forms/multiple-select.blade.php
+++ src/Ccm/views/components/forms/multiple-select.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->merge(['class' => 'mt-1 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white min-h-9']) }}
+<div {{ $attributes->merge(['class' => 'mt-1 block w-full rounded-md border-0 py-1 pl-3 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white min-h-9']) }}
      @click="$focus.first()"
 >
     <div x-data="textArray({ elements: @entangle('filter.value') })">
