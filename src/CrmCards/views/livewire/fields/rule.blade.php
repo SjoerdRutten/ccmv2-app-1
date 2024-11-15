739f9200536b4d56d7674fecea7465f1dfd5b911
Ray.nl B.V <sjoerd@ray.nl>
2024-11-15 16:00:37 +0100

    Extension rules

--- src/CrmCards/views/livewire/fields/rule.blade.php
+++ src/CrmCards/views/livewire/fields/rule.blade.php
@@ -4,23 +4,25 @@
 
         <x-ccm::forms.select label="Type regel" x-model="rule.type">
             <option></option>
-            @if (Arr::get($correctors['extensions']))
+            @if (Arr::get($correctors, 'extensions'))
                 <option value="extensions">Correctie via extensie</option>
             @endif
             <option value="regex">Correctie m.b.v. patroonherkenning</option>
             <option value="casing">Correctie van hoofd- en kleine letters gebruik</option>
             <option value="removing">Verwijderen karakters en witruimte</option>
         </x-ccm::forms.select>
-        <div x-show="rule.type === 'extensions'">
-            <x-ccm::forms.select label="Extensie regel" x-model="rule.corrector">
-                <option></option>
-                @foreach ($correctors['extensions'] AS $key => $corrector)
-                    <option value="{{ $corrector::class }}">
-                        {{ $corrector->name }}
-                    </option>
-                @endforeach
-            </x-ccm::forms.select>
-        </div>
+        @if (Arr::get($correctors, 'extensions'))
+            <div x-show="rule.type === 'extensions'">
+                <x-ccm::forms.select label="Extensie regel" x-model="rule.corrector">
+                    <option></option>
+                    @foreach ($correctors['extensions'] AS $key => $corrector)
+                        <option value="{{ $corrector::class }}">
+                            {{ $corrector->name }}
+                        </option>
+                    @endforeach
+                </x-ccm::forms.select>
+            </div>
+        @endif
         <div x-show="rule.type === 'removing'">
             <x-ccm::forms.select label="Verwijder de volgende karakters" x-model="rule.corrector">
                 <option></option>
