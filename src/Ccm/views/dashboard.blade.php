6b65f38f613ab1c25056a6e109b9762fe75b1239
Ray.nl B.V <sjoerd@ray.nl>
2024-12-11 08:38:37 +0100

    Remove typesense cards

--- src/Ccm/views/dashboard.blade.php
+++ src/Ccm/views/dashboard.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <x-ccm::cards.cards cols="4">
-        <x-ccm::dashboard.typesense-collections-card/>
-        @if (Auth::user()->isAdmin)
-            <x-ccm::dashboard.typesense-memory-card/>
-        @endif
+        {{--        <x-ccm::dashboard.typesense-collections-card/>--}}
+        {{--        @if (Auth::user()->isAdmin)--}}
+        {{--            <x-ccm::dashboard.typesense-memory-card/>--}}
+        {{--        @endif--}}
     </x-ccm::cards.cards>
 @endsection
