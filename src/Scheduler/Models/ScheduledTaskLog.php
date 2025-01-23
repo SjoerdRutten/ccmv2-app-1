b34bec20ace508470bef7b17e8e1d346e3574b67
Ray.nl B.V <sjoerd@ray.nl>
2025-01-23 16:00:06 +0100

    Prune per month

--- src/Scheduler/Models/ScheduledTaskLog.php
+++ src/Scheduler/Models/ScheduledTaskLog.php
@@ -3,12 +3,14 @@
 namespace Sellvation\CCMV2\Scheduler\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Sellvation\CCMV2\Environments\Traits\HasEnvironment;
 
 class ScheduledTaskLog extends Model
 {
     use HasEnvironment;
+    use Prunable;
 
     protected $fillable = [
         'is_success',
@@ -19,5 +21,10 @@ class ScheduledTaskLog extends Model
     public function scheduledTask(): BelongsTo
     {
         return $this->belongsTo(ScheduledTask::class);
+    }
+
+    public function prunable()
+    {
+        return $this->created_at->isBefore(now()->subMonth());
     }
 }
