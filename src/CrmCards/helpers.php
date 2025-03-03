14f1b1990dce74819f44fcdc98f51dd342b0b66e
Ray.nl B.V <sjoerd@ray.nl>
2025-03-03 16:28:01 +0100

    crmDataDate

--- src/CrmCards/helpers.php
+++ src/CrmCards/helpers.php
@@ -2,11 +2,36 @@
 
 declare(strict_types=1);
 
+use Carbon\Carbon;
+use Illuminate\Support\Arr;
+
 if (! function_exists('crmData')) {
     function crmData(string $dataField, $default = null): mixed
     {
         if ($crmCard = \Context::get('crmCard', null)) {
             return Arr::get($crmCard->data, $dataField, $default);
+        }
+
+        return $default;
+    }
+}
+
+if (! function_exists('crmDataDate')) {
+    function crmDataDate(string $dataField, ?string $format = null, $default = null, $locale = 'nl_NL'): mixed
+    {
+        if ($crmCard = \Context::get('crmCard', null)) {
+            try {
+                $date = Carbon::parse(Arr::get($crmCard->data, $dataField));
+                $date->locale($locale);
+            } catch (\Exception $e) {
+                return $default;
+            }
+
+            if ($format) {
+                return $date->isoFormat($format);
+            }
+
+            return $date;
         }
 
         return $default;
