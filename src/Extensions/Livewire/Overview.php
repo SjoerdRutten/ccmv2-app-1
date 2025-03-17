c00b3890df63fa9b1eccf3fc19bfd565dec07b06
Ray.nl B.V <sjoerd@ray.nl>
2025-03-17 13:27:07 +0100

    extension overview

--- src/Extensions/Livewire/Overview.php
+++ src/Extensions/Livewire/Overview.php
@@ -2,6 +2,7 @@
 
 namespace Sellvation\CCMV2\Extensions\Livewire;
 
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 use Sellvation\CCMV2\Extensions\Models\Extension;
@@ -10,9 +11,25 @@ class Overview extends Component
 {
     use WithPagination;
 
+    #[Url]
+    public array $filter = [
+        'q' => '',
+        'event' => '',
+        'job' => '',
+    ];
+
     private function getExtensions()
     {
         return Extension::query()
+            ->when(! empty($this->filter['q']), function ($query) {
+                $query->where('name', 'like', "%{$this->filter['q']}%");
+            })
+            ->when(! empty($this->filter['event']), function ($query) {
+                $query->where('event', "{$this->filter['event']}");
+            })
+            ->when(! empty($this->filter['job']), function ($query) {
+                $query->where('job', "{$this->filter['job']}");
+            })
             ->paginate();
     }
 
@@ -21,6 +38,8 @@ class Overview extends Component
         return view('extensions::extensions.overview')
             ->with([
                 'extensions' => $this->getExtensions(),
+                'events' => \ExtensionService::getExtensionEvents(),
+                'jobs' => \ExtensionService::getExtensionJobs(),
             ]);
     }
 }
--- src/Extensions/views/extensions/overview.blade.php
+++ src/Extensions/views/extensions/overview.blade.php
@@ -4,15 +4,42 @@
             <x-slot:actions>
                 <x-ccm::buttons.add route="admin::extensions::add">Extensie toevoegen</x-ccm::buttons.add>
             </x-slot:actions>
+            <div class="flex gap-4">
+                <x-ccm::forms.input
+                        name="filter.q"
+                        wire:model.live="filter.q"
+                >
+                    Zoeken
+                </x-ccm::forms.input>
+                <x-ccm::forms.select label="Event" wire:model.live="filter.event">
+                    <option></option>
+                    @foreach ($events AS $class => $name)
+                        <option value="{{ $class }}">
+                            {{ class_basename($class) }} -
+                            {{ $name }}
+                        </option>
+                    @endforeach
+                </x-ccm::forms.select>
+                <x-ccm::forms.select label="Job" wire:model.live="filter.job">
+                    <option></option>
+                    @foreach ($jobs AS $class => $name)
+                        <option value="{{ $class }}">
+                            {{ class_basename($class) }} -
+                            {{ $name }}
+                        </option>
+                    @endforeach
+                </x-ccm::forms.select>
+
+            </div>
         </x-ccm::pages.intro>
         <x-ccm::tables.table>
             <x-slot:thead>
                 <x-ccm::tables.th :first="true">ID</x-ccm::tables.th>
                 <x-ccm::tables.th>Actief</x-ccm::tables.th>
-                <x-ccm::tables.th>Starttijd</x-ccm::tables.th>
-                <x-ccm::tables.th>Eindtijd</x-ccm::tables.th>
                 <x-ccm::tables.th>Naam</x-ccm::tables.th>
                 <x-ccm::tables.th>Omschrijving</x-ccm::tables.th>
+                <x-ccm::tables.th>Event</x-ccm::tables.th>
+                <x-ccm::tables.th>Job</x-ccm::tables.th>
                 <x-ccm::tables.th :link="true">Acties</x-ccm::tables.th>
             </x-slot:thead>
             <x-slot:tbody>
@@ -21,15 +48,26 @@
                         <x-ccm::tables.td :first="true">{{ $extension->id }}</x-ccm::tables.td>
                         <x-ccm::tables.td>
                             <x-ccm::is-active :is-active="$extension->show_active"/>
-                        </x-ccm::tables.td>
-                        <x-ccm::tables.td>
-                            {{ $extension->start_at?->toDateTimeString() }}
-                        </x-ccm::tables.td>
-                        <x-ccm::tables.td>
-                            {{ $extension->end_at?->toDateTimeString() }}
+                            <x-slot:sub>
+                                {!! $extension->start_at?->toDateTimeString() ?? '&infin;' !!}
+                                t/m
+                                {!! $extension->end_at?->toDateTimeString() ?? '&infin;' !!}
+                            </x-slot:sub>
                         </x-ccm::tables.td>
                         <x-ccm::tables.td>{{ $extension->name }}</x-ccm::tables.td>
                         <x-ccm::tables.td>{{ $extension->description }}</x-ccm::tables.td>
+                        <x-ccm::tables.td>
+                            {{ $extension->event::getName() }}
+                            <x-slot:sub>
+                                {{ class_basename($extension->event) }}
+                            </x-slot:sub>
+                        </x-ccm::tables.td>
+                        <x-ccm::tables.td>
+                            {{ $extension->job::getName() }}
+                            <x-slot:sub>
+                                {{ class_basename($extension->job) }}
+                            </x-slot:sub>
+                        </x-ccm::tables.td>
                         <x-ccm::tables.td :link="true">
                             <x-ccm::tables.edit-link :href="route('admin::extensions::edit', $extension)"/>
                         </x-ccm::tables.td>
