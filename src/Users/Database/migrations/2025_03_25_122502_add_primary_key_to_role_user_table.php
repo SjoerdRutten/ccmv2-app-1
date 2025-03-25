e4304cc30613d2aad9237be5e977d4972ce1cbb3
Ray.nl B.V <sjoerd@ray.nl>
2025-03-25 13:26:21 +0100

    add primary key to role_user table

---
+++ src/Users/Database/migrations/2025_03_25_122502_add_primary_key_to_role_user_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('role_user', function (Blueprint $table) {
+            $table->dropColumn('id');
+            $table->primary(['role_id', 'user_id']);
+        });
+    }
+};
