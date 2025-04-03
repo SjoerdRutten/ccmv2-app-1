6617c45b784529940d7b26deed248e101fc67bd3
Ray.nl B.V <sjoerd@ray.nl>
2025-04-03 16:22:18 +0200

    Remove unused cards from mongo

---
+++ src/Ccmp/Console/Commands/RemoveCrmCardsFromMongoCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Sellvation\CCMV2\Ccmp\Console\Commands;
+
+use Illuminate\Console\Command;
+use Sellvation\CCMV2\CrmCards\Models\CrmCard;
+use Sellvation\CCMV2\CrmCards\Models\CrmCardMongo;
+
+class RemoveCrmCardsFromMongoCommand extends Command
+{
+    protected $signature = 'remove:crm-cards-from-mongo';
+
+    protected $description = 'Remove CRM Cards from MongoDB which doesn\'t exists anymore in Mysql';
+
+    public function handle(): void
+    {
+        $bar = $this->output->createProgressBar(CrmCardMongo::count());
+
+        $count = 0;
+        foreach (CrmCardMongo::cursor() as $crmCardMongo) {
+            $bar->advance();
+
+            if (! CrmCard::where('id', '=', $crmCardMongo->id)->exists()) {
+                $count++;
+                $crmCardMongo->delete();
+            }
+        }
+
+        $bar->finish();
+
+        $this->info($count.' cards removed from MongoDB');
+    }
+}
